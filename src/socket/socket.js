b4a9bacfa3e6fea3bdf807185e22ee6fee5dfeeb
Akinur Rahman <akinurrahmanofficial@gmail.com>
2025-01-02 18:23:18 +0530

    socket bug fix

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,15 +1,16 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
-import {app} from './app.js'
-
+import { app } from "../app.js";
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
+
 
 const userSocketMap = {}; // Map to store user socket connections
 
